f2a28139d7ba67d75a6baa26fb3f03a89e01e813
Andreas Thomas <dev@chronark.com>
2023-03-02 12:00:01 +0100

    build: remove rome

--- .contentlayer/generated/index.mjs
+++ .contentlayer/generated/index.mjs
@@ -4,8 +4,8 @@ export { isType } from 'contentlayer/client'
 
 // NOTE During development Contentlayer imports from `.mjs` files to improve HMR speeds.
 // During (production) builds Contentlayer it imports from `.json` files to improve build performance.
-import allPages from './Page/_index.json' assert { type: 'json' }
-import allProjects from './Project/_index.json' assert { type: 'json' }
+import { allPages } from './Page/_index.mjs'
+import { allProjects } from './Project/_index.mjs'
 
 export { allPages, allProjects }
 
