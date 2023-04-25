c648faca6b23ec4a56cd570c3816468d25a44235
Adam Hughes <adamjordanhughes@gmail.com>
2023-04-24 21:42:15 -0400

    Remove hydration error, remove unneeded npm packages, and build the resume component

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,16 +1,31 @@
+"use client";
 import { Navigation } from "../components/nav";
-// import { Card } from "../components/card";
-
+import React, { useEffect } from 'react';
 
 export default function Example() {
-	return (
-	  <div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
-		<Navigation />
-		<div className="container flex items-center justify-center min-h-screen px-4 mx-auto md:pt-20">
-		  <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-8 sm:mt-0 lg:gap-16 lg:grid-cols-3">
-            <div className="text-white">the resume view</div>
-		  </div>
-		</div>
-	  </div>
-	);
-  }
+    const pdfURL = 'https://drive.google.com/uc?id=1yNS4kMzA_4n1-EhRksbgRJsoQQWmEFSc'
+
+    return (
+        <div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+            <Navigation />
+            <div className="container flex items-center justify-center min-h-screen px-4 mx-auto md:pt-20">
+                <div className="w-full max-w-2xl mx-auto mt-8 sm:mt-0">
+                    <iframe
+                        id="pdf-frame"
+                        src={pdfURL}
+                        title="Front End Developer Resume"
+                        className="w-full h-[500px] md:h-128 lg:min-h-[600px] xl:h-[700px] 2xl:h-[800px] border-0"
+                        fallback={`
+                            <p>
+                                Your browser doesn't support iframes.{' '}
+                                <a href=${pdfURL}>Click here to view the PDF file.</a>
+                            </p>
+                        `}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
