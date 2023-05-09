c4da35240db0c468219d9cc60be83e416d7a066d
Tom Schindler <schindlertom@icloud.com>
2023-05-09 20:12:59 +0200

    fix missing .com in header.tsx

--- app/projects/[slug]/header.tsx
+++ app/projects/[slug]/header.tsx
@@ -67,7 +67,7 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 								views,
 							)}
 						</span>
-						<Link target="_blank" href="https://twitter/chronark_">
+						<Link target="_blank" href="https://twitter.com/chronark_">
 							<Twitter
 								className={`w-6 h-6 duration-200 hover:font-medium ${
 									isIntersecting
