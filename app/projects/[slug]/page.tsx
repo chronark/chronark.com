84e339a180a00656491db2099303e2f680ac7a25
Carlos Vicent Domenech <carvido1@Carloss-Mac-mini.local>
2023-05-20 18:04:34 +0200

    feat: to do small refactor to publis in vercel

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -4,7 +4,7 @@ import { Mdx } from "@/app/components/mdx";
 import { Header } from "./header";
 import "./mdx.css";
 import { ReportView } from "./view";
-import { Redis } from "@upstash/redis";
+// import { Redis } from "@upstash/redis";
 
 export const revalidate = 60;
 
@@ -14,7 +14,7 @@ type Props = {
 	};
 };
 
-const redis = Redis.fromEnv();
+// const redis = Redis.fromEnv();
 
 export async function generateStaticParams(): Promise<Props["params"][]> {
 	return allProjects
@@ -32,12 +32,12 @@ export default async function PostPage({ params }: Props) {
 		notFound();
 	}
 
-	const views =
-		(await redis.get<number>(["pageviews", "projects", slug].join(":"))) ?? 0;
+	// const views =
+	// 	(await redis.get<number>(["pageviews", "projects", slug].join(":"))) ?? 0;
 
 	return (
 		<div className="bg-zinc-50 min-h-screen">
-			<Header project={project} views={views} />
+			<Header project={project} views={0} />
 			<ReportView slug={project.slug} />
 
 			<article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">
