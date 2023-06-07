88af2d844fb91ce3e982c73c5564fd623e2d1801
Dominik Kugelmann <9637909+daallgeier@users.noreply.github.com>
2023-06-07 19:11:41 +0200

    Update page.tsx
    change chronark to tangerine

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
 				quantity={100}
 			/>
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				chronark
+				tangerine 🍊
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
