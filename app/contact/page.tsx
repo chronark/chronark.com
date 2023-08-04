96e1ed3d318b11b71237ffd9e92f158c5ef7224b
jackmonaco20 <jackmonaco20@yahoo.com>
2023-08-04 14:42:18 -0400

    margin changes for additional social cards

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -49,7 +49,7 @@ export default function Example() {
 		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
+				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16 lg:mt-24">
 					{socials.map((s) => (
 						<Card>
 							<Link
