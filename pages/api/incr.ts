a78770ebc074025c8a7ec2dfa4353a74b3c30f17
Thomas O'Brien <41043317+Tom0Brien@users.noreply.github.com>
2023-08-15 00:52:21 +1000

    Fix import

--- pages/api/incr.ts
+++ pages/api/incr.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const redis = Redis.fromEnv();
 export const config = {
 	runtime: "edge",
 };
@@ -31,16 +30,6 @@ export default async function incr(req: NextRequest): Promise<NextResponse> {
 		const hash = Array.from(new Uint8Array(buf))
 			.map((b) => b.toString(16).padStart(2, "0"))
 			.join("");
-
-		// deduplicate the ip for each slug
-		const isNew = await redis.set(["deduplicate", hash, slug].join(":"), true, {
-			nx: true,
-			ex: 24 * 60 * 60,
-		});
-		if (!isNew) {
-			new NextResponse(null, { status: 202 });
-		}
 	}
-	await redis.incr(["pageviews", "projects", slug].join(":"));
 	return new NextResponse(null, { status: 202 });
 }
