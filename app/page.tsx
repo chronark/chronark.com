157b9f024c786b95a1434dc3958696a500f77ed4
Hadi Samadzad <h.samadzad@gmail.com>
2023-10-08 14:48:00 +0100

    Change home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
+import { Navigation } from "./components/nav";
 
 const navigation = [
   { name: "Blog", href: "/blog" },
@@ -9,36 +10,22 @@ const navigation = [
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <nav className="my-16 animate-fade-in">
-        <ul className="flex items-center justify-center gap-4">
-          {navigation.map((item) => (
-            <Link
-              key={item.href}
-              href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-            >
-              {item.name}
-            </Link>
-          ))}
-        </ul>
-      </nav>
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <Particles
-        className="absolute inset-0 -z-10 animate-fade-in"
-        quantity={100}
-      />
-      <h1 className="z-10 text-xl text-transparent duration-100 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        Hadi Samadzad
-      </h1>
+    <>
+      <Navigation />
 
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
-          Hi, my name is Hadi, I'm a backend developer living in England. I ♥
-          programming at night.
-        </h2>
+      <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+        <Particles className="absolute inset-0 -z-10" quantity={100} />
+        <h1 className="z-10 text-xl cursor-default text-white font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+          Hadi Samadzad
+        </h1>
+
+        <div className="my-16 text-center">
+          <h2 className="text-sm text-zinc-500 ">
+            Hi, my name is Hadi, I'm a backend developer living in England. I ♥
+            programming at night.
+          </h2>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
