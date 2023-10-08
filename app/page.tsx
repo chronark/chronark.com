5cc52ef7b55552957483ea862b2b57724081924d
Hadi Samadzad <h.samadzad@gmail.com>
2023-10-08 16:22:05 +0100

    Update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -14,13 +14,14 @@ export default function Home() {
       <Navigation />
 
       <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-        <Particles className="absolute inset-0 -z-10" quantity={100} />
-        <h1 className="z-10 text-xl cursor-default text-white font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-          Hadi Samadzad
+        <Particles className="absolute inset-0 -z-10" quantity={150} />
+        <text className=" text-orange-300 text-3xl">I'm a</text>
+        <h1 className="  text-white font-display sm:text-4xl md:text-6xl">
+          Software Developer
         </h1>
 
         <div className="my-16 text-center">
-          <h2 className="text-sm text-zinc-500 ">
+          <h2 className="text-md text-zinc-500 ">
             Hi, my name is Hadi, I'm a backend developer living in England. I ♥
             programming at night.
           </h2>
