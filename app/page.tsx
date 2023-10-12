a3853e97feed66b6b1af66eb4bcf3834aee2c1c0
Ryan Malani <ryanmalani24@gmail.com>
2023-10-12 03:54:07 -0400

    small fixes for mobile (#23)
    * fix: font size overflows contact boxes on mobile
    
    * fix: navigation header overlaps with page heading
    
    * chore: added x margin and swapped to word break for cleaner look

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
 			<nav className="my-16 animate-fade-in">
-				<ul className="flex items-center justify-center gap-4">
+				<ul className="flex flex-wrap items-center justify-center gap-4">
 					{navigation.map((item) => (
 						<Link
 							key={item.href}
@@ -34,7 +34,7 @@ export default function Home() {
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
-				<h2 className="text-sm text-zinc-500 ">
+				<h2 className="text-sm text-zinc-500 mx-6">
 					Hi, my name is Andreas, I'm building serverless and open source
 					solutions at{" "}
 					<Link
@@ -45,8 +45,8 @@ export default function Home() {
 						Upstash
 					</Link>
 
-					<br />
-					and working on{" "}
+					
+					<wbr /> and working on{" "}
 					<Link
 						target="_blank"
 						href="https://unkey.dev"
