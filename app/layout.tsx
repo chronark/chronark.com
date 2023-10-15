a41f00be6e126662605b4b38289c3a80ce6217f4
Juliano Warpechowski <julianogwarp@gmail.com>
2023-10-14 22:09:37 -0300

    commit change chronark to julianogwarp

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
 	title: {
-		default: "chronark.com",
-		template: "%s | chronark.com",
+		default: "julianogwarp.com",
+		template: "%s | julianogwarp.com",
 	},
 	description: "Software engineer at upstash.com and founder of planetfall.io",
 	openGraph: {
-		title: "chronark.com",
+		title: "julianogwarp.com",
 		description:
 			"Software engineer at upstash.com and founder of planetfall.io",
-		url: "https://chronark.com",
-		siteName: "chronark.com",
+		url: "https://julianogwarp.com",
+		siteName: "julianogwarp.com",
 		images: [
 			{
-				url: "https://chronark.com/og.png",
+				url: "https://julianogwarp.com/og.png",
 				width: 1920,
 				height: 1080,
 			},
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
 		},
 	},
 	twitter: {
-		title: "Chronark",
+		title: "Julianogwarp",
 		card: "summary_large_image",
 	},
 	icons: {
