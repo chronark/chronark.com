d0f21639ab14a30ba3c5db95d3fc5c16695a6ea1
Dody <dodyags@gmail.com>
2023-11-06 00:03:12 +0700

    Fix issue #6 home page text fill color is not full (#39)
    * fix issue #6 home page text fill color is not full
    
    * fix tabspace

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
 				className="absolute inset-0 -z-10 animate-fade-in"
 				quantity={100}
 			/>
-			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text pb-4">
 				chronark
 			</h1>
 
