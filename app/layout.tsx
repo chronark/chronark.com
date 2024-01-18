757142b3a1470c534043e3a30e69026b2481efe3
Lucas Bonafé <lucas@bonafe.org>
2024-01-18 15:47:26 -0300

    Fix built-in next/font and YAMLParse errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "../global.css";
-import { Inter } from "@next/font/google";
-import LocalFont from "@next/font/local";
+import { Inter } from "next/font/google";
+import LocalFont from "next/font/local";
 import { Metadata } from "next";
 import { Analytics } from "./components/analytics";
 
