0a1b9fdfe1b4316d8472ebf1c1d7ccbf75435a0f
nayrasleao <nayraleao.souza@gmail.com>
2024-03-08 22:45:34 -0400

    doc: change image

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <img className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text " src="logo-branca.png" alt="Logo Hashteck soluções tecnologicas." />
+      <img className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text " src="logo-azul.png" alt="Logo Hashteck soluções tecnologicas." />
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">        
