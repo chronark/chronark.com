5e4be3f1afa8c29aa26681fac6b39159d097a95c
Devollox <122895078+Devollox@users.noreply.github.com>
2024-06-16 21:21:07 +0300

    Update page.tsx

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -37,7 +37,7 @@ export default async function PostPage({ params }: Props) {
 
   return (
     <div className="bg-zinc-50 min-h-screen">
-      <Header project={project} views={views} />
+      <Header project={project} views={views + 1} />
       <ReportView slug={project.slug} />
 
       <article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">
