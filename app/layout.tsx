45537281b0cea42f01ca73a317af15d435c637c0
Jordan Weatherford <jrw821@gmail.com>
2024-08-04 10:23:26 -0700

    first commit. basic text changes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "sossy.com",
+    template: "%s | sossy.com",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
+    title: "sossy.com",
     description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+      "Founder, Master Pilot & Lead Photographer for Path to Ground LLC",
+    url: "https://sossy.com",
+    siteName: "sossy.com",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://sossy.com/og.png",
         width: 1920,
         height: 1080,
       },
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "Path to Ground",
     card: "summary_large_image",
   },
   icons: {
--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        chronark
+        sossy
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
@@ -41,8 +41,8 @@ export default function Home() {
             href="https://unkey.dev"
             className="underline duration-500 hover:text-zinc-300"
           >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+            link.here
+          </Link> link text goes here.
         </h2>
       </div>
     </div>
